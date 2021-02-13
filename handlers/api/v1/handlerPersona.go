b60b5eae1d4d1373a09c9d98311deb4b849054f1
gracefurya <gfuruyavaldivia@gmail.com>
2021-02-12 23:54:59 -0400

    se crearon los servidores

---
+++ handlers/api/v1/handlerPersona.go
@@ -0,0 +1,56 @@
+package v1
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"../../../models"
+	"github.com/gorilla/mux"
+)
+
+//GetPersonaByID devuelve un dato del tipo persona
+func GetPersonaByID(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		vars := mux.Vars(r)
+		re, err := strconv.Atoi(vars["idpersona"])
+		if err != nil {
+			log.Println("Error en GetPersonaById: ", err)
+			models.SendUnprocessableEntity(w)
+			return
+		}
+		persona, err2 := models.ObtenerUsuario(re)
+		if err2 != nil {
+			log.Println("Error en GetPersonaById: ", err)
+			models.SendUnprocessableEntity(w)
+			return
+		}
+
+		if persona.IDPersona == 0 {
+			models.SendNotFound(w)
+			return
+		}
+
+		models.SendData(w, persona)
+
+	}
+}
+
+//AddPersona agregar persona
+func AddPersona(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		persona := models.Persona{}
+		decoder := json.NewDecoder(r.Body)
+		if err := decoder.Decode(&persona); err != nil {
+			models.SendUnprocessableEntity(w)
+		}
+
+		err := persona.AgregarUsuario()
+
+		if err != nil {
+			models.SendUnprocessableEntity(w)
+		}
+
+	}
+}
--- principal.go
+++ principal.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
+	v1 "./handlers/api/v1"
 	"./models"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	crearTablas()
 	fmt.Println("hola")
+	mux := mux.NewRouter()
+	mux.HandleFunc("/api/v1/persona/{idpersona:[0-9]+}", v1.GetPersonaByID).Methods("GET")
+	mux.HandleFunc("/api/v1/persona", v1.AddPersona).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
 
 func crearTablas() {
@@ -17,4 +26,5 @@ func crearTablas() {
 	models.CrearTablaZonaVerde()
 	models.CrearTablaArbol()
 	models.CrearTablaEstadoArbol()
+
 }
