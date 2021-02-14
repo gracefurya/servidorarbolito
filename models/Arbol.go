2e9cbaea11629189c8034c437d48ceb401262f3a
Christian Marban Callisaya <marbanchristian@gmail.com>
2021-02-13 20:42:12 -0400

    se agrego la creacion del arbol y tambien la optencion de los arboles por persona

--- models/Arbol.go
+++ models/Arbol.go
@@ -15,11 +15,14 @@ type Arbol struct {
 	IDZonaVerde string    `json:"idzonaverde"`
 }
 
+//Arboles array de arbol
+type Arboles []Arbol
+
 var queryArbol = `CREATE TABLE if NOT EXISTS arbol(
 	idarbol int primary KEY not null AUTO_INCREMENT,
 	nombre varchar(60) not null,
 	tipo varchar(60) not null,
-	fechanac date,
+	fechanac DATETIME(0),
 	dirfoto varchar(120),
 	persona_id int not null,
 	CONSTRAINT fk_arbol_persona
@@ -32,4 +35,52 @@ var queryArbol = `CREATE TABLE if NOT EXISTS arbol(
 //CrearTablaArbol se crea una tabla para el objeto arbol
 func CrearTablaArbol() {
 	EjecutarExec(queryArbol)
+}
+
+//AgregarArbol agregamos un arbol a la base de datos
+func (a *Arbol) AgregarArbol() error {
+	query := `INSERT INTO arbol(nombre,tipo,fechanac,dirfoto,persona_id,zonaverde_id) VALUE(?,?,?,?,?,?)`
+	var fn = FechaAString(a.FechaNac)
+	if _, err := EjecutarExec(query, &a.Nombre, &a.Tipo, fn, &a.DirFoto, &a.IDPersona, &a.IDZonaVerde); err != nil {
+		return err
+	}
+	return nil
+}
+
+//ObtenerArbolByID crea un arbol relacionada a una persona y a su zona verde
+func ObtenerArbolByID(id int) (*Arbol, error) {
+	query := `SELECT idarbol,nombre,tipo,fechanac,dirfoto,persona_id,zonaverde_id FROM arbol
+	WHERE idarbol=?`
+	rows, err := EjecutarQuery(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	arbol := &Arbol{}
+	if rows.Next() {
+		rows.Scan(&arbol.IDArbol, &arbol.Nombre, &arbol.Tipo, &arbol.FechaNac, &arbol.DirFoto, &arbol.IDPersona, &arbol.IDZonaVerde)
+	}
+
+	return arbol, nil
+}
+
+//ObtenerArbolesByPersonaID obtiene todos los arboles relacionadas a una persona
+func ObtenerArbolesByPersonaID(idpersona int) (Arboles, error) {
+	query := `SELECT idarbol,nombre,tipo,fechanac,dirfoto,persona_id,zonaverde_id FROM arbol
+	WHERE persona_id=?`
+	rows, err := EjecutarQuery(query, idpersona)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	arboles := Arboles{}
+	for rows.Next() {
+		arbol := &Arbol{}
+		rows.Scan(&arbol.IDArbol, &arbol.Nombre, &arbol.Tipo, &arbol.FechaNac, &arbol.DirFoto, &arbol.IDPersona, &arbol.IDZonaVerde)
+		arboles = append(arboles, *arbol)
+	}
+
+	return arboles, nil
 }
